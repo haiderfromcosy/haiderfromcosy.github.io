f065dda2e494655bf2b67a83693e14af2e4fb5d3
haiderfromcosy <lionandpeace@gmail.com>
2021-07-23 14:03:15 +0100

    we can now receive hover events

--- scripts.js
+++ scripts.js
@@ -1,34 +1,9 @@
 let cosyLogo = document.querySelector('#cosy-logo');
-let cosyFMLogo =document.querySelector('#cosy-fm-logo');
+let cosyFMLogo = document.querySelector('#cosy-fm-logo');
 let haiderHaider = document.querySelector('#haiderHeader');
 
-
-cosyLogo.addEventListener('mouseover', hoverOne);
-cosyLogo.addEventListener('mouseout', hoverOneFinish)
-cosyFMLogo.addEventListener('mouseover', hoverTwo);
-cosyFMLogo.addEventListener('mouseout', hoverTwoFinish);
-
-
-
-function hoverOne(){
-    document.body.classList.add('red');
-}
-
-function hoverOneFinish(){
-    document.body.classList.remove('red');
-}
-
-function hoverTwo(){
-    document.body.classList.add('green');
-}
-function hoverTwoFinish(){
-    document.body.classList.remove('green');
-}
-
-function hoverThree(){
-
-}
-
-function hoverFour(){
-
-}
+document.querySelectorAll('.links a').forEach( function(element){
+    element.addEventListener('mouseover', function(){
+        console.log('hello');
+    })
+} )
