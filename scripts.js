1731073ef6799ebfb57dab09f4aa239f2fe87f29
haiderfromcosy <lionandpeace@gmail.com>
2021-07-23 14:16:12 +0100

    we have different code running for each icon

--- scripts.js
+++ scripts.js
@@ -4,6 +4,16 @@ let haiderHaider = document.querySelector('#haiderHeader');
 
 document.querySelectorAll('.links a').forEach( function(element){
     element.addEventListener('mouseover', function(){
-        console.log('hello');
+        
+        if ( element.id == "cosy-link" ) {
+            alert('I just hovered over COSY')
+        } else if ( element.id == "youtube-link" ) {
+            alert('YouTube Link')
+        } else if ( element.id == "instagram-link" ) {
+            alert('Instagram Link')
+        } else if ( element.id == 'spotify-link' ){
+            alert('Spotify link')
+        }
+
     })
 } )
